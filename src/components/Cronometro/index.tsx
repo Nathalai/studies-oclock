6166405d8af741de5ea98e80ca5e1a497d9c8a15
Nathalia Luiza Schedler Calza <nlscalza@gmail.com>
2022-06-13 10:26:03 -0300

    feat: developed task selection

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,13 +1,25 @@
 import Botao from "../Button";
 import Relogio from "./Relogio";
 import style from "./Cronometro.module.scss"
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tema";
+import { useState } from "react";
 
-export default function Cronometro() {
+interface Props {
+    selecionado: ITarefa | undefined
+}
+
+export default function Cronometro({selecionado}: Props) {  
+    const [tempo, setTempo] = useState<number>();
+    if (selecionado?.tempo) {
+        setTempo(tempoParaSegundos(selecionado.tempo));
+    }
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>
                Escolha um card e inicie o cronômetro
-            </p>
+               Tempo: {tempo}
+            </p>            
             <div className={style.relogioWrapper}>
                 <Relogio />
             </div>
--- src/components/List/Item/index.tsx
+++ src/components/List/Item/index.tsx
@@ -1,13 +1,34 @@
 import { ITarefa } from "../../../types/tema";
-import style from "../Lista.module.scss";
+import style from "./Item.module.scss";
 
-export default function Item({ tema, tempo, selecionado, completado, id }: ITarefa) {
-    console.log("item atual", {tema, tempo, selecionado, completado, id});
+interface Props extends ITarefa{
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+export default function Item(
+    {
+        tema, 
+        tempo, 
+        selecionado, 
+        completado, 
+        id, 
+        selecionaTarefa
+    }: Props) {    
     return (
-        <li className={style.item}>
+        <li 
+            className={`${style.item} ${selecionado ? style.itemSelecionado : ""}`} 
+            onClick={() => selecionaTarefa(
+                {
+                tema, 
+                tempo, 
+                selecionado, 
+                completado, 
+                id
+                }
+            )}
+        >
             <h3> {tema} </h3>
             <span> {tempo} </span>
         </li>
-    )
-    
+    )    
 }
