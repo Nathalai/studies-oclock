7357d3ca495f77d391821e9e1fda7b0f145ebcbd
Nathalia Luiza Schedler Calza <nlscalza@gmail.com>
2022-06-23 14:02:57 -0300

    feat: adds task selection and countdown timer

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,34 @@
 import React from "react";
 import style from './Botao.module.scss';
 
-class Botao extends React.Component <{ type?: "button" | "submit" | "reset" | undefined, children: any }> {
+interface Props {
+    type?: "button" | "submit" | "reset" | undefined
+    onClick?: () => void
+    children?: React.ReactNode
+}
+
+function Botao({onClick, type, children}: Props) {
+    return (
+        <button onClick={onClick} type={type} className={style.botao}>
+            {children}
+        </button>
+    )
+}
+
+//Class Components (não é mais muito utilizado, foi substituído pelo function components):
+/* class Botao extends React.Component <{ 
+    type?: "button" | "submit" | "reset" | undefined 
+    children: React.ReactNode
+    onClick?: () => void
+}> {
     render() {
-        const { type = "button" } = this.props;
+        const { type = "button", onClick } = this.props;
         return (
-            <button type={type} className={style.botao}>
+            <button onClick={onClick} type={type} className={style.botao}>
                 {this.props.children}
             </button>
         )
     }
-}
+} */
 
 export default Botao;
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,10 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { ITarefa } from "../../types/tema";
 import Botao from "../Button";
 import style from "./Formulario.module.scss";
 import { v4 as uuidv4 } from "uuid";
 
-class Formulario extends React.Component<{
+interface Props {
+    setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}
+
+function Formulario({setTarefas}: Props) {
+    const [tema, setTema] = useState("");
+    const [tempo, setTempo] = useState("00:00");
+
+    function adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+        evento.preventDefault();
+        setTarefas(tarefasAntigas => 
+            [
+                ...tarefasAntigas, 
+                {
+                    tema, 
+                    tempo,
+                    selecionado: false,
+                    completado: false,
+                    id: uuidv4()
+                }
+            ]);
+        setTema("");
+        setTempo("00:00");        
+    }
+
+    return(
+        <form className={style.novaTarefa} onSubmit={adicionarTarefa}>
+            <div className= {style.inputContainer}>
+                <label htmlFor="tarefa">Adicione um novo estudo</label>
+                <input 
+                    type="text" 
+                    name="tarefa"
+                    value={tema}
+                    onChange={evento => setTema(evento.target.value)}
+                    id="tarefa" 
+                    placeholder="O que você vai estudar?" 
+                    required 
+                />
+            </div>
+            <div className= {style.inputContainer}>
+                <label htmlFor="tempo">Tempo</label>
+                <input 
+                    type="time" 
+                    step="1" 
+                    name="tempo" 
+                    value={tempo}
+                    onChange={evento => setTempo(evento.target.value)}
+                    id="tempo" 
+                    min="00:00:00" 
+                    max="01:30:00"
+                    placeholder="Por quanto tempo você vai estudar?"
+                    required
+                />
+            </div>
+            <Botao type="submit">
+                Adicionar
+            </Botao>                   
+        </form>
+    )
+}
+
+//Class Components (não é mais muito utilizado, foi substituído pelo function components):
+/* class Formulario1 extends React.Component<{
     setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
 }> {
     state = {
@@ -66,6 +128,6 @@ class Formulario extends React.Component<{
             </form>
         )
     }
-}
+} */
 
 export default Formulario;
